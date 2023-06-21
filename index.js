a605d872ec264104d2014b5ec2fc6d09b789475d
Kevin04c <kevincespedes2004@gmail.com>
2023-06-21 16:05:28 -0500

    first

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+
+const db = new PrismaClient()
+
+const PORT = 8080 || process.env.PORT
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await db.products.findMany()
+    res.status(200).json({
+      products,
+    })
+  } catch (error) {
+    res.status(400).json({
+      message: 'Error al cargar los productos'
+    })
+  }
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`App running on PORT ${PORT}`)
+})
